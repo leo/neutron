f26d66ab2bee4b7d6521f5e206695187a43c7912
Leo Lamprecht <mindrun@icloud.com>
2017-12-26 15:21:32 +0100

    Render help and version

--- bin/mini.js
+++ bin/mini.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
+const parse = require('arg')
+const package = require('../package')
+const help = require('../lib/help')
+
+const args = parse({
+  '--version': Boolean,
+  '--help': Boolean,
+  '-v': '--version',
+  '-h': '--help'
+})
+
+const sub = new Set(args._)
+
+if (args['--version']) {
+  console.log(package.version)
+  process.exit(0)
+}
+
+if (args['--help'] || sub.has('help')) {
+  console.log(help)
+  process.exit(0)
+}
+
 console.log('I am mini')
