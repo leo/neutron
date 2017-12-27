ec4da63def25e9155f5b3d4a4db5c344f5a0dbaa
Leo Lamprecht <mindrun@icloud.com>
2017-12-27 16:49:27 +0100

    Ability to specify a custom path

--- bin/pack.js
+++ bin/pack.js
@@ -5,6 +5,7 @@ const { resolve } = require('path')
 
 // Packages
 const parse = require('arg')
+const { pathExists } = require('fs-extra')
 
 // Utilities
 const package = require('../package')
@@ -14,7 +15,7 @@ const clean = require('../lib/clean')
 const setInfo = require('../lib/info')
 const createBundle = require('../lib/bundle')
 
-const args = parse({
+const { _, ...args } = parse({
   '--version': Boolean,
   '--help': Boolean,
   '--output': String,
@@ -23,20 +24,31 @@ const args = parse({
   '-o': '--output'
 })
 
-if (args['--version']) {
-  console.log(package.version)
-  process.exit(0)
-}
-
-const sub = new Set(args._)
-
-if (args['--help'] || sub.has('help')) {
-  console.log(help)
-  process.exit(0)
-}
+const subSpec = [ 'help' ]
+const sub = subSpec[subSpec.indexOf(_[0])]
 
 const main = async () => {
-  const output = resolve(process.cwd(), args['--output'] || 'out')
+  let path = process.cwd()
+
+  if (!sub && _[0]) {
+    const resolved = resolve(path, _[0])
+
+    if (await pathExists(resolved)) {
+      path = resolved
+    }
+  }
+
+  if (args['--version']) {
+    console.log(package.version)
+    process.exit(0)
+  }
+
+  if (args['--help'] || sub === 'help') {
+    console.log(help)
+    process.exit(0)
+  }
+
+  const output = resolve(path, args['--output'] || 'out')
   const appName = 'Now'
 
   // Ensure we can start fresh by cleaning up the old output
