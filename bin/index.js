1d7012ca2fe62703d0a48f9ffc56689e1283d84e
Leo Lamprecht <mindrun@icloud.com>
2018-01-07 01:42:19 +0100

    Everything corrected, no starting needed

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 
 // Packages
 const parse = require('arg')
+const { grey } = require('chalk')
 
 // Utilities
 const package = require('../package')
@@ -9,12 +10,15 @@ const spinner = require('../lib/spinner')
 const help = require('../lib/help')
 
 // Parse the supplied commands and options
-const { _: sub, ...args } = parse({
+const { _, ...args } = parse({
   '--version': Boolean,
   '--help': Boolean,
   '-v': '--version',
   '-h': '--help'
 })
+
+// Check if a sub command was supplied
+let run = _[0]
 
 // Output the package's version if
 // the `--version was supplied
@@ -23,10 +27,16 @@ if (args['--version']) {
   process.exit(0)
 }
 
-// Just like in Next.js, we'll fall back to
-// the `dev` sub command if none was specified
-if (!sub[0]) {
-  sub[0] = 'dev'
+// As there's no sub command needed for starting the
+// application in production (Electron is set to load
+// the main process code directly), we need
+// to ensure to fall back to `dev` in those cases
+
+// The same happens if no sub command was defined and
+// by checking if `dev` was supplied, we also make
+// sure to log the same usage information in all cases
+if (!run || run === 'start' || run === 'dev') {
+  run = 'dev'
 
   if (args['--help']) {
     console.log(help.dev)
@@ -36,21 +46,20 @@ if (!sub[0]) {
 
 // Try to load the sub command
 try {
-  require(`./${sub[0]}.js`)()
+  require(`./${run}.js`)()
 } catch (err) {
   // Ensure missing modules from required files
   // still lead to an error
   if (!err.stack.includes('bin/index')) {
     throw err
-    return
   }
 
   // If it wasn't possible, we might not support it
   if (err.code === 'MODULE_NOT_FOUND') {
-    spinner.fail('The specified sub command is not supported')
+    spinner.fail(`The sub command ${grey(`\`${_[0]}\``)} is not supported`)
     return
   }
 
   // ...or it was just the program's fault
-  spinner.fail(`Not able to load the "${sub[0]}" sub command`)
+  throw err
 }
